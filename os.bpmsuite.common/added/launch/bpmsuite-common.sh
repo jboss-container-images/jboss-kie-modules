a2279b5a749ab9f08a8fc6928bf52c89935b5f26
Ruben Romero Montes <rromerom@redhat.com>
2018-04-17 14:11:43 +0200

    Added multi-maven user creation
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- os.bpmsuite.common/added/launch/bpmsuite-common.sh
+++ os.bpmsuite.common/added/launch/bpmsuite-common.sh
@@ -7,10 +7,15 @@ source $JBOSS_HOME/bin/launch/logging.sh
 function prepareEnv() {
     # please keep these in alphabetical order
     unset KIE_ADMIN_PWD
+    unset KIE_ADMIN_ROLES
     unset KIE_ADMIN_USER
     unset KIE_MBEANS
     unset KIE_SERVER_CONTROLLER_PWD
+    unset KIE_SERVER_CONTROLLER_ROLES
     unset KIE_SERVER_CONTROLLER_USER
+    unset MAVEN_REPO_PASSWORD
+    unset MAVEN_REPO_ROLES
+    unset MAVEN_REPO_USERNAME
 }
 
 function configureEnv() {
@@ -20,6 +25,7 @@ function configureEnv() {
 function configure() {
     configure_admin_security
     configure_controller_security
+    configure_maven_security
     configure_maven_settings
     configure_mbeans
 }
@@ -27,20 +33,56 @@ function configure() {
 function configure_admin_security() {
     local kieAdminUser=$(find_env "KIE_ADMIN_USER" "adminUser")
     local kieAdminPwd=$(find_env "KIE_ADMIN_PWD" "admin1!")
-    ${JBOSS_HOME}/bin/add-user.sh -a --user "${kieAdminUser}" --password "${kieAdminPwd}" --role "kie-server,rest-all,admin,kiemgmt,Administrators"
-    if [ "$?" -ne "0" ]; then
-        log_error "Failed to create admin user \"${kieAdminUser}\""
-        log_error "Exiting..."
-        exit
-    fi
+    local kieAdminRoles=$(find_env "KIE_ADMIN_ROLES" "kie-server,rest-all,admin,kiemgmt,Administrators")
+
+    add_eap_user "admin" "${kieAdminUser}" "${kieAdminPwd}" "${kieAdminRoles}"
 }
 
 function configure_controller_security() {
     local kieServerControllerUser=$(find_env "KIE_SERVER_CONTROLLER_USER" "controllerUser")
     local kieServerControllerPwd=$(find_env "KIE_SERVER_CONTROLLER_PWD" "controller1!")
-    ${JBOSS_HOME}/bin/add-user.sh -a --user "${kieServerControllerUser}" --password "${kieServerControllerPwd}" --role "kie-server,rest-all,guest"
+    local kieServerControllerRoles=$(find_env "KIE_SERVER_CONTROLLER_ROLES" "kie-server,rest-all,guest")
+
+    add_eap_user "controller" "${kieServerControllerUser}" "${kieServerControllerPwd}" "${kieServerControllerRoles}"
+}
+
+function add_maven_user () {
+  local username=${2:-$(find_prefixed_env $1 "REPO_USERNAME")}
+  local password=$(find_prefixed_env $1 "REPO_PASSWORD" "maven1!")
+  local roles=$(find_prefixed_env $1 "REPO_ROLES")
+  add_eap_user maven ${username:-"mavenUser"} ${password} ${roles}
+}
+
+function configure_maven_security() {
+    # multiple repos scenario
+    IFS=',' read -a multi_mavenRepoPrefixes <<< ${MAVEN_REPOS}
+    for prefix in ${multi_mavenRepoPrefixes[@]}; do
+        prefix="${prefix}_MAVEN"
+        local username=$(find_prefixed_env $prefix "REPO_USERNAME")
+        if [[ -n ${username} ]]; then
+            add_maven_user $prefix $username
+            mavenUserAdded=true
+        fi
+    done
+    # single repo scenario
+    # we will only create a default maven user if no single nor multi maven username(s) were specified
+    if [[ -z ${mavenUserAdded} ]]; then
+        add_maven_user "MAVEN"
+    fi
+}
+
+function add_eap_user() {
+    local eapType="${1}"
+    local eapUser="${2}"
+    local eapPwd="${3}"
+    local eapRoles="${4}"
+    if [ "x${eapRoles}" != "x" ]; then
+        ${JBOSS_HOME}/bin/add-user.sh -a --user "${eapUser}" --password "${eapPwd}" --role "${eapRoles}"
+    else
+        ${JBOSS_HOME}/bin/add-user.sh -a --user "${eapUser}" --password "${eapPwd}"
+    fi
     if [ "$?" -ne "0" ]; then
-        log_error "Failed to create controller user \"${kieServerControllerUser}\""
+        log_error "Failed to create ${eapType} user \"${eapUser}\""
         log_error "Exiting..."
         exit
     fi
@@ -71,4 +113,3 @@ function configure_mbeans() {
     fi
     JBOSS_BPMSUITE_ARGS="${JBOSS_BPMSUITE_ARGS} -Dkie.mbeans=${KIE_MBEANS} -Dkie.scanner.mbeans=${KIE_MBEANS}"
 }
-
