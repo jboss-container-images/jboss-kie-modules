62c5dca06232d403d11fb295f2e5a190f73f123f
David Ward <dward@redhat.com>
2018-09-18 14:26:12 -0400

    [RHDM-739] optaweb-employee-rostering templates incomplete
    Signed-off-by: David Ward <dward@redhat.com>

--- jboss-kie-optaweb-common/added/launch/jboss-kie-optaweb-common.sh
+++ jboss-kie-optaweb-common/added/launch/jboss-kie-optaweb-common.sh
@@ -2,6 +2,7 @@
 
 source "${JBOSS_HOME}/bin/launch/launch-common.sh"
 source "${JBOSS_HOME}/bin/launch/logging.sh"
+source "${JBOSS_HOME}/bin/launch/jboss-kie-security.sh"
 
 #function prepareEnv() {
     # please keep these in alphabetical order
@@ -12,8 +13,14 @@ function configureEnv() {
 }
 
 function configure() {
+    configure_optaweb_security
     configure_optaweb_generator
     configure_property_replacement
+}
+
+function configure_optaweb_security() {
+    # add eap users (see jboss-kie-security.sh)
+    add_kie_admin_user
 }
 
 function configure_optaweb_generator() {
@@ -23,5 +30,5 @@ function configure_optaweb_generator() {
 }
 
 function configure_property_replacement() {
-    sed -i.bak "s/<spec-descriptor-property-replacement>false/<spec-descriptor-property-replacement>true/g" $JBOSS_HOME/standalone/configuration/standalone-openshift.xml
+    sed -i "s/<spec-descriptor-property-replacement>false/<spec-descriptor-property-replacement>true/g" $CONFIG_FILE
 }
