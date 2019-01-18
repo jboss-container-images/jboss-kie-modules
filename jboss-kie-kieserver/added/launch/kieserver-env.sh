3ade3f1392c9ff65120a018c4b36c68844fbde20
Evan Zhang <evan.zhang@redhat.com>
2019-01-18 11:59:19 -0500

    [KIECLOUD-96] Enable KieServer OpenShiftStartupStrategy and Workbench integration [JBPM-7834] Provide OpenShiftStartupStrategy - Enhancements related to RHPAM templates and others

--- jboss-kie-kieserver/added/launch/kieserver-env.sh
+++ jboss-kie-kieserver/added/launch/kieserver-env.sh
@@ -12,6 +12,9 @@ function getKieJavaArgs() {
     for launchJar in ${JBOSS_HOME}/bin/launch/*.jar; do
         kieClassPath="${kieClassPath}:${launchJar}"
     done
+    for javaxJar in ${JBOSS_HOME}/modules/system/layers/base/javax/validation/api/main/*.jar; do
+        kieClassPath="${kieClassPath}:${javaxJar}"
+    done
     echo "-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN -jar ${JBOSS_HOME}/jboss-modules.jar -mp ${JBOSS_HOME}/modules -dep javax.enterprise.api,javax.inject.api,sun.jdk -cp ${kieClassPath}"
 }
 
