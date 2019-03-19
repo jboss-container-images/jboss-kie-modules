0337318d3fc1d74760005cdcff3c3d98f9f65141
Filippe Spolti <fspolti@redhat.com>
2019-03-18 22:41:00 -0300

    fix templates doc generation If a DeploymentConfig has a containers section without envs and ports the generation will fail.

--- tools/gen-template-doc/gen_template_docs.py
+++ tools/gen-template-doc/gen_template_docs.py
@@ -60,6 +60,7 @@ LINKS = {"rhdm71-kieserver-openshift:1.0": "../../../kieserver/image.yaml[`rhdm-
          "rhdm72-kieserver-openshift:1.1": "../../../kieserver/image.yaml[`rhdm-7/rhdm72-kieserver-openshift`]",
          "rhdm73-kieserver-openshift:1.0": "../../../kieserver/image.yaml[`rhdm-7/rhdm73-kieserver-openshift`]",
          "rhpam71-kieserver-openshift:1.0": "../../../kieserver/image.yaml[`rhpam-7/rhpam71-kieserver-openshift`]",
+         "rhpam71-kieserver-openshift:1.1": "../../../kieserver/image.yaml[`rhpam-7/rhpam71-kieserver-openshift`]",
          "rhpam72-kieserver-openshift:1.0": "../../../kieserver/image.yaml[`rhpam-7/rhpam72-kieserver-openshift`]",
          "rhpam72-kieserver-openshift:1.1": "../../../kieserver/image.yaml[`rhpam-7/rhpam72-kieserver-openshift`]",
          "rhpam73-kieserver-openshift:1.0": "../../../kieserver/image.yaml[`rhpam-7/rhpam73-kieserver-openshift`]",
@@ -261,11 +262,15 @@ def getVariableInfo(parameters, name, env, field):
 
 def createParameterTable(data):
     text = ""
+    containerEnvs = ""
     for param in data["parameters"]:
         if u"\u2019" in param["description"]:
             param["description"] = param["description"].replace(u"\u2019", "'")
-        containerEnvs = [d["spec"]["template"]["spec"]["containers"][0]["env"] for d in data["objects"] if
-                         d["kind"] == "DeploymentConfig"]
+        try:
+            containerEnvs = [d["spec"]["template"]["spec"]["containers"][0]["env"] for d in data["objects"] if d["kind"] == "DeploymentConfig"]
+        except KeyError, e:
+            pass
+
         parameters = [item for sublist in containerEnvs for item in sublist]
         envVar = getVariableInfo(parameters, param["name"], [], "name")
         value = param["value"] if param.get("value") else getVariableInfo(data['parameters'], param["name"], [], "value")
@@ -390,14 +395,14 @@ def createContainerTable(data, table):
                              + " ".join(container["readinessProbe"]["exec"]["command"]) \
                              + "\n----\n")
 
-        elif table == "ports":
+        elif table == "ports" and "ports" in container:
             text += "\n." + str(len(container["ports"])) + "+| `" + deployment + "`"
             ports = container["ports"]
             for p in ports:
                 columns = ["name", "containerPort", "protocol"]
                 columns = [str(p[col]) if p.get(col) else "--" for col in columns]
                 text += buildRow(columns)
-        elif table == "env":
+        elif table == "env" and "env" in container:
             environment = container["env"]
             text += "\n." + str(len(environment)) + "+| `" + deployment + "`"
             for env in environment:
