c971ce0ce3ff6fa79f9900cd5801e9f87bde80b9
Mauricio Magnani <mmagnani@localhost.localdomain>
2019-05-07 15:54:37 -0300

    Test Module

---
+++ jboss-kie-common/configure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Move the parent EAP readiness and liveness probe scripts and install child versions
+set -e
+
+SCRIPT_DIR=$(dirname $0)
+ADDED_DIR=${SCRIPT_DIR}/added
+
+# Necessary to permit running with a randomised UID
+chown -R jboss:root $SCRIPT_DIR
+chmod -R g+rwX $SCRIPT_DIR
+
+mv $JBOSS_HOME/bin/jboss-kie-common.sh $JBOSS_HOME/bin/jboss-kie-common.sh
+
+cp -p "$ADDED_DIR/jboss-kie-common.sh" $JBOSS_HOME/bin/
+
+chmod ug+x $JBOSS_HOME/bin/jboss-kie-common.sh
