d6ca77e0fbfa75ee70c493191d714b51cd2c6915
Evan Zhang <evan.zhang@redhat.com>
2019-06-17 09:31:47 -0400

    [RHPAM-2008] Reverting jboss-kie-modules PR 262 (#267)
    Signed-off-by: Evan Zhang <evan.zhang@redhat.com>

--- jboss-kie-kieserver/added/launch/jboss-kie-kieserver-hooks.sh
+++ jboss-kie-kieserver/added/launch/jboss-kie-kieserver-hooks.sh
@@ -56,7 +56,8 @@ if [ -n ${WORKBENCH_SERVICE_NAME} -a -n "${KIE_SERVER_ID}" ]; then
         fi
 
     elif [ ${kieReplicas} -gt 0 ]; then
-        log_info "KIE Server Replicas is ${kieReplicas}"
+        log_info "KIE Server Replicas is ${kieReplicas}, updating ${KIE_SERVER_ID} configMap to USED."
+        update_config_map "{\"metadata\":{\"labels\":{\"services.server.kie.org/kie-server-state\":\"USED\"}}}" "${kieDCName}"
 
         if [ "${controllerServiceHost}x" != "x" -a "${controllerServicePort}x" != "x" -a ${controllerReplicas} -gt 0  ]; then
             # curl command may be hanging a bit during dc pod starting up; need to update
