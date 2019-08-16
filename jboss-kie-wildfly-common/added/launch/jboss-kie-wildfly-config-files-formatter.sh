353dca003ea608fa0e0ce8e75452aefbbb698bfe
Filippe Spolti <fspolti@redhat.com>
2019-08-16 16:56:24 -0300

    [KIECLOUD-306] - Pretty format generated *.-openshift.xml files
    Signed-off-by: Filippe Spolti <fspolti@redhat.com>

---
+++ jboss-kie-wildfly-common/added/launch/jboss-kie-wildfly-config-files-formatter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# KIECLOUD-306 - Pretty format generated *.-openshift.xml files
+function postConfigure() {
+    format_xml
+}
+
+function format_xml() {
+    mv $CONFIG_FILE $CONFIG_FILE.bkp
+    #format and write the new file
+    xmllint --format $CONFIG_FILE.bkp > $CONFIG_FILE
+}
+
