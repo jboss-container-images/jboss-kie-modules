a7ca1d9aac80c0a5d7d0f003f0215d9bfe1f15ea
Ruben Romero Montes <rromerom@redhat.com>
2020-01-16 12:04:54 +0100

    [RHPAM-2576] Install drivers using repo manager
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- jboss-kie-process-migration/configure.sh
+++ jboss-kie-process-migration/configure.sh
@@ -6,7 +6,6 @@ SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 PROCESS_MIGRATION_DIR=/opt/${JBOSS_PRODUCT}
 JDBC_DRIVERS=${PROCESS_MIGRATION_DIR}/drivers
-SOURCES_DIR="/tmp/artifacts"
 
 # Add custom launch script and dependent scripts/libraries/snippets
 cp -p ${ADDED_DIR}/openshift-launch.sh ${PROCESS_MIGRATION_DIR}/
@@ -18,10 +17,8 @@ mkdir -p ${CONFIG_DIR}
 cp -r ${ADDED_DIR}/configuration/* ${CONFIG_DIR}
 
 mkdir -p ${JDBC_DRIVERS}
-cp -p ${SOURCES_DIR}/postgresql*.jar ${JDBC_DRIVERS}
-cp -p ${SOURCES_DIR}/mariadb*.jar ${JDBC_DRIVERS}
-ln -s ${JDBC_DRIVERS}/postgresql*.jar ${JDBC_DRIVERS}/postgresql.jar
-ln -s ${JDBC_DRIVERS}/mariadb-java-client*.jar ${JDBC_DRIVERS}/mariadb-java-client.jar
+link /usr/lib/java/mariadb-java-client.jar $JDBC_DRIVERS/mariadb-java-client.jar
+link /usr/share/java/postgresql-jdbc.jar $JDBC_DRIVERS/postgresql-jdbc.jar
 
 # Necessary to permit running with a randomised UID
 chown -R jboss:root ${PROCESS_MIGRATION_DIR}
