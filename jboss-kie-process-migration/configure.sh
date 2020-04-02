3d726286748fabaa6650a2707916f10e02cafebe
Ruben Romero Montes <rromerom@redhat.com>
2020-04-02 17:44:06 +0200

    [RHPAM-2576] Install mariadb and postgres jdbc drivers (#345)
    * [RHPAM-2576] Install mariadb and postgres jdbc drivers
    * [RHPAM-2576] Install drivers using repo manager
    * [RHPAM-2576] Fix postgresql link
    
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- jboss-kie-process-migration/configure.sh
+++ jboss-kie-process-migration/configure.sh
@@ -5,6 +5,7 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 PROCESS_MIGRATION_DIR=/opt/${JBOSS_PRODUCT}
+JDBC_DRIVERS=${PROCESS_MIGRATION_DIR}/drivers
 
 # Add custom launch script and dependent scripts/libraries/snippets
 cp -p ${ADDED_DIR}/openshift-launch.sh ${PROCESS_MIGRATION_DIR}/
@@ -14,6 +15,10 @@ cp -r ${ADDED_DIR}/launch/* ${LAUNCH_DIR}
 
 mkdir -p ${CONFIG_DIR}
 cp -r ${ADDED_DIR}/configuration/* ${CONFIG_DIR}
+
+mkdir -p ${JDBC_DRIVERS}
+link /usr/lib/java/mariadb-java-client.jar ${JDBC_DRIVERS}/mariadb-java-client.jar
+link /usr/share/java/postgresql-jdbc/postgresql.jar ${JDBC_DRIVERS}/postgresql-jdbc.jar
 
 # Necessary to permit running with a randomised UID
 chown -R jboss:root ${PROCESS_MIGRATION_DIR}
