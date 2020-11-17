66dda2755ef7318f34b0d465a991fe9c69af8035
spolti <fspolti@redhat.com>
2020-11-17 17:32:09 -0300

    [RHPAM-3299] - BC image not using PV to store its local .m2/repository
    Signed-off-by: spolti <fspolti@redhat.com>

--- jboss-kie-workbench/added/openshift-launch.sh
+++ jboss-kie-workbench/added/openshift-launch.sh
@@ -30,13 +30,14 @@ CONFIGURE_SCRIPTS=(
   $JBOSS_HOME/bin/launch/keycloak.sh
   $JBOSS_HOME/bin/launch/deploymentScanner.sh
   $JBOSS_HOME/bin/launch/ports.sh
-  $JBOSS_HOME/bin/launch/maven-settings.sh
   $JBOSS_HOME/bin/launch/access_log_valve.sh
   $JBOSS_HOME/bin/launch/mp-config.sh
   $JBOSS_HOME/bin/launch/tracing.sh
   $JBOSS_HOME/bin/launch/filters.sh
   $JBOSS_HOME/bin/launch/jboss-kie-wildfly-common.sh
   $JBOSS_HOME/bin/launch/jboss-kie-workbench.sh
+  # RHPAM-3299 - jboss-kie-wildfly-common.sh needs to run before maven-settings.sh so MAVEN_LOCAL_REPO can be correctly set
+  $JBOSS_HOME/bin/launch/maven-settings.sh
   $JBOSS_HOME/bin/launch/jboss-kie-wildfly-config-files-formatter.sh
   /opt/run-java/proxy-options
 )
