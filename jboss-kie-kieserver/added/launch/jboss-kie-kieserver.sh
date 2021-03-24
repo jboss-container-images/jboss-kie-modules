faa7bff314e67c45a59e5fe2361f99e9853684fb
Fast Chaffeur <desmax74@users.noreply.github.com>
2021-03-24 14:38:06 +0100

    Update jboss-kie-kieserver/added/launch/jboss-kie-kieserver.sh
    Co-authored-by: Spolti <filippespolti@gmail.com>

--- jboss-kie-kieserver/added/launch/jboss-kie-kieserver.sh
+++ jboss-kie-kieserver/added/launch/jboss-kie-kieserver.sh
@@ -725,7 +725,7 @@ function generate_random_id() {
 function configure_jbpm_cluster(){
     if [ "${KIE_SERVER_JBPM_CLUSTER^^}" = "TRUE" ]; then
       configure_jbpm_cache
-      log_info "Kie Server's cluster for JBPM fail over enabled"
+      log_info "KIE Server's cluster for Jbpm failover is enabled."
     else
       log_info "Kie Server's cluster for JBPM fail over disabled"
     fi
