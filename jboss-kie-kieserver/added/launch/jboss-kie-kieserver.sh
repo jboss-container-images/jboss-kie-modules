e2020d20e7746308d562febb81ed62d42eb4d4e7
Fast Chaffeur <desmax74@users.noreply.github.com>
2021-05-17 14:52:20 -0400

    Update jboss-kie-kieserver/added/launch/jboss-kie-kieserver.sh
    Co-authored-by: Spolti <filippespolti@gmail.com>

--- jboss-kie-kieserver/added/launch/jboss-kie-kieserver.sh
+++ jboss-kie-kieserver/added/launch/jboss-kie-kieserver.sh
@@ -752,7 +752,8 @@ function configure_jbpm_cache() {
         </replicated-cache>\
         </cache-container>\n<cache-container name="server" aliases="singleton cluster" default-cache="default" module="org.wildfly.clustering.server">#g' ${CONFIG_FILE}
 
-    sed -i 's#<system export="true">#<module name="org.infinispan" services="export"/>\n<module name="org.jgroups"/>\n<system export="true">#g' ${JBOSS_HOME}/standalone/deployments/ROOT.war/WEB-INF/jboss-deployment-structure.xml
+    sed -i  's#<module name="org.apache.xerces"/>#& \n      <module name="org.infinispan" services="export"/>\n      <module name="org.jgroups"/>#' ${JBOSS_HOME}/standalone/deployments/ROOT.war/WEB-INF/jboss-deployment-structure.xml
+
 
     if [ -d "/opt/kie/dependencies/jbpm-clustering" ] ;then
       mv /opt/kie/dependencies/jbpm-clustering/kie-server-services-jbpm-cluster-* ${JBOSS_HOME}/standalone/deployments/ROOT.war/WEB-INF/lib
