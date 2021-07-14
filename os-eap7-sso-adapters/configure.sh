08b5c44844fcbd7dc035a21a7e80b8abcb411ebe
spolti <fspolti@redhat.com>
2021-07-14 15:50:45 -0300

    [KIECLOUD-548] - Adjust permissions on os-eap-sso-adapters module
    Signed-off-by: spolti <fspolti@redhat.com>

--- os-eap7-sso-adapters/configure.sh
+++ os-eap7-sso-adapters/configure.sh
@@ -7,5 +7,8 @@ SOURCES_DIR="/tmp/artifacts"
 unzip -o "$SOURCES_DIR"/rh-sso-7.4.5-eap7-adapter.zip -d $JBOSS_HOME
 unzip -o "$SOURCES_DIR"/rh-sso-7.4.5-saml-eap7-adapter.zip -d $JBOSS_HOME
 
-chown -R jboss:root $JBOSS_HOME
-chmod -R g+rwX $JBOSS_HOME
+for dir in $JBOSS_HOME/docs/licenses-rh-sso $JBOSS_HOME/modules/system/add-ons $JBOSS_HOME/bin; do
+    chown -R jboss:root $dir
+    chmod -R g+rwX $dir
+done
+
