5b23f5769045a2f77e543e24fe7e3e4d04f657cd
spolti <fspolti@redhat.com>
2021-09-06 10:08:39 -0300

    [KIECLOUD-576] - RHDM KIE Server image: /opt/kie directory not found
    Signed-off-by: spolti <fspolti@redhat.com>

--- jboss-kie-kieserver/configure.sh
+++ jboss-kie-kieserver/configure.sh
@@ -48,6 +48,7 @@ chmod -R 755 ${KIE_DIR}
 
 # /opt/kie directory
 KIE_HOME_DIR=/opt/kie
+mkdir -p ${KIE_HOME_DIR}
 
 # Necessary to permit running with a randomised UID
 for dir in $JBOSS_HOME/bin $HOME $KIE_HOME_DIR; do
