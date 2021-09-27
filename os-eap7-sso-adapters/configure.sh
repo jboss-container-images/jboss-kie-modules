42276da82f3de1d382ffabb97886515cf6dd12f1
Spolti <fspolti@redhat.com>
2021-09-27 18:49:39 -0300

    [KIECLOUD-565] - Update RHSSO EAP adatpers from 7.4.5 to 7.5.0 (#490)
    Signed-off-by: spolti <fspolti@redhat.com>

--- os-eap7-sso-adapters/configure.sh
+++ os-eap7-sso-adapters/configure.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 SOURCES_DIR="/tmp/artifacts"
 
-unzip -o "$SOURCES_DIR"/rh-sso-7.4.5-eap7-adapter.zip -d $JBOSS_HOME
-unzip -o "$SOURCES_DIR"/rh-sso-7.4.5-saml-eap7-adapter.zip -d $JBOSS_HOME
+unzip -o "$SOURCES_DIR"/rh-sso-7.5.0-eap7-adapter-dist.zip -d $JBOSS_HOME
+unzip -o "$SOURCES_DIR"/rh-sso-7.5.0-saml-eap7-adapter-dist.zip -d $JBOSS_HOME
 
 for dir in $JBOSS_HOME/docs/licenses-rh-sso $JBOSS_HOME/modules/system/add-ons $JBOSS_HOME/bin; do
     chown -R jboss:root $dir
