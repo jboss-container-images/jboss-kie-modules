802c18c0063c3dd8b053d344f0fc09e72eb14fba
Filippe Spolti <fspolti@redhat.com>
2023-03-22 21:58:56 -0300

    [RHPAM-4633] - Add Red Hat GA maven repository to the active profiles on settings.xml (#615)
    Signed-off-by: Spolti <fspolti@redhat.com>

--- jboss-kie-workbench/configure.sh
+++ jboss-kie-workbench/configure.sh
@@ -23,3 +23,7 @@ for dir in $JBOSS_HOME/bin $HOME $KIE_HOME_DIR; do
     chmod -R g+rwX $dir
 done
 
+# Enable the jboss-eap-repository maven profile by default.
+# There is a activation property for command line mvn commands (-Dcom.redhat.xpaas.repo.redhatga)
+# however it seems not to apply to the kiesoup-maven integration for embedded integration, thus enable by default.
+sed -i 's|<!-- ### active profiles ### -->|<activeProfile>jboss-eap-repository</activeProfile>\n\    <!-- ### active profiles ### -->|' ${HOME}/.m2/settings.xml
