cf0b2aceb9cf33545b96a7c73e17339ceecfe45d
Spolti <fspolti@redhat.com>
2023-09-06 14:21:15 -0300

    [KIECLOUD-735] - Prepare RHPAM repositories for 7.13.5 release
    Signed-off-by: Spolti <fspolti@redhat.com>

--- tools/build-overrides/build-overrides.sh
+++ tools/build-overrides/build-overrides.sh
@@ -898,7 +898,7 @@ main() {
     local products_valid=( all \
         rhpam rhpam-businesscentral rhpam-businesscentral-monitoring rhpam-controller rhpam-kieserver rhpam-process-migration rhpam-smartrouter rhpam-dashbuilder)
     local product_default="all"
-    local version_example="7.13.4"
+    local version_example="7.13.5"
     local default_dir_example="/tmp/${build_tool}/${build_type_default}/${build_date_default}/${version_example}"
     local default_dir
     local artifacts_dir
