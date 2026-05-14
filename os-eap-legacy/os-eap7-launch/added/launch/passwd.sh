e2f54ad169c6fc280d99d26960e5432b8695f9fc
Saumya Singh <saumyatheengineer@gmail.com>
2026-05-14 14:19:51 +0530

    RHPAM-5145- Workaround fix: CVE-2025-58713 /etc/passwd permissions change (#703)
    Co-authored-by: Saumya Singh <sausingh@redhat.com>

--- os-eap-legacy/os-eap7-launch/added/launch/passwd.sh
+++ os-eap-legacy/os-eap7-launch/added/launch/passwd.sh
@@ -7,5 +7,6 @@ function configure() {
 function configure_passwd() {
   sed "/^jboss/s/[^:]*/$(id -u)/3" /etc/passwd > /tmp/passwd
   cat /tmp/passwd > /etc/passwd
+  chmod 644 /etc/passwd
   rm /tmp/passwd
 }
