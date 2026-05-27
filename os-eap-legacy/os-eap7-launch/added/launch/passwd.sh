e6de48d53ab6c0f1ebb849867990ec63084af23a
Jakub Schwan <jschwan@redhat.com>
2026-05-27 16:39:31 +0200

    Revert "RHPAM-5145- Workaround fix: CVE-2025-58713 /etc/passwd permissions change (#702)"
    This reverts commit b0c68703b6a57f01cfa19f938cb2a04b7925df2c.

--- os-eap-legacy/os-eap7-launch/added/launch/passwd.sh
+++ os-eap-legacy/os-eap7-launch/added/launch/passwd.sh
@@ -7,6 +7,5 @@ function configure() {
 function configure_passwd() {
   sed "/^jboss/s/[^:]*/$(id -u)/3" /etc/passwd > /tmp/passwd
   cat /tmp/passwd > /etc/passwd
-  chmod 644 /etc/passwd
   rm /tmp/passwd
 }
